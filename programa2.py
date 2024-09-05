0a33e6a1be657c0fd2af126d15bd4643b2ba7049
Glhalton Amaro <125841549+Glhalton@users.noreply.github.com>
2024-09-04 21:55:43 -0300

    Comentarios e o loop com timer

--- programa2.py
+++ programa2.py
@@ -1,6 +1,10 @@
+
+#Importação dos módulos de acesso a modificação de arquivos
 import os
 import shutil
+import time
 
+#Criação da função que irá verificar os arquivos da pasta e exclui-lo
 def excluir_arquivos(pasta):
     if not os.path.exists(pasta):
         print(f"\nA pasta {pasta} não existe\n")
@@ -16,13 +20,22 @@ def excluir_arquivos(pasta):
 
     print("Todos os arquivos foram excluídos!")
 
-pastaTemp = 'C:/Users/UserTI/Documents/Codes/Python/TEMP'
-pastaRaiz = 'C:/Users/UserTI/Documents/Codes/Python'
+#Diretório que será verificado e o que será excluido 
+pastaTemp = 'C:/Users/TI/Documents/PASTA TEMPORARIA'
+pastaRaiz = 'C:/Users/TI/Documents/PASTA RAIZ'
 
-itens = os.listdir(pastaRaiz)
-num_arqui = sum(1 for item in itens if os.path.isfile(os.path.join(pastaRaiz, item)))
+#Executa o codigo em loop
+while True:
 
-if num_arqui > 10:
-    excluir_arquivos(pastaTemp)
-else:
-    print(f"\nA pasta {pastaRaiz} está fnucionando bem.\n")
+    #Lista todos os itens na pasta raiz em seguida realiza a contagem
+    itens = os.listdir(pastaRaiz)
+    num_arqui = sum(1 for item in itens if os.path.isfile(os.path.join(pastaRaiz, item)))
+
+    #Verifica a quantidade de arquivos para exlcluir
+    if num_arqui > 10:
+        excluir_arquivos(pastaTemp)
+    else:
+        print(f"\nA pasta {pastaRaiz} está funcionando bem.\n")
+
+    #Cria um delay
+    time.sleep(5)
